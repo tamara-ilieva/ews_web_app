4e5adac2287856b47c828851d7eb55d3b84d93f2
Ivan <ivan.trifunov@outlook.com>
2024-05-29 14:56:44 +0200

    Dashboard Images Table Added (Backend)

--- backend/app/models/image.py
+++ backend/app/models/image.py
@@ -6,12 +6,13 @@ from sqlmodel import SQLModel, Field
 class Image(SQLModel, table=True):
     __tablename__ = 'images'
     id: int = Field(primary_key=True, index=True)
-    disease: int = Field(foreign_key="diseases.id", nullable=True)
+    predicted_disease: int = Field(foreign_key="diseases.id", nullable=True)
+    is_sick: bool = Field(nullable=True)
+    predicted_disease_human_input: int = Field(foreign_key="diseases.id", nullable=True)
+    is_sick_human_input: bool = Field(nullable=True)
     file_url: str
     created_at: datetime = Field(nullable=False)
     updated_at: datetime = Field(nullable=False)
-    # is_sick: bool = False
-    # confirmed: bool = False
 
 class ImageOut(SQLModel):
     id: int
